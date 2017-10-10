dc4ffb95335a9bd2b683bb4973b42ea494c99b23
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-10 15:32:28 +0200

    feat(classes/exceptions/validation_error.js) class created

---
+++ src/classes/exceptions/validation_error.js
@@ -0,0 +1,10 @@
+// @flow
+import Exception from './base';
+
+export default class ValidationError extends Exception {
+  constructor(message: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = message || 'A validation error occurred.';
+  }
+}
