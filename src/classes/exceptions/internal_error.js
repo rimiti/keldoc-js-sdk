6478d8bd2ac6ff027dbea0bc582f5e9be49ac9da
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-10 15:33:18 +0200

    feat(classes/exceptions/internal_error.js) class created

---
+++ src/classes/exceptions/internal_error.js
@@ -0,0 +1,10 @@
+// @flow
+import Exception from './base';
+
+export default class InternalError extends Exception {
+  constructor(message: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = message || 'An error occurred with our API.';
+  }
+}
