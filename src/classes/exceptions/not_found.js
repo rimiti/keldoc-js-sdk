567402673bd4d8b1802f915edee30b1b178a5bf0
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-10 15:28:38 +0200

    feat(classes/exceptions/not_found.js) class created

---
+++ src/classes/exceptions/not_found.js
@@ -0,0 +1,10 @@
+// @flow
+import Exception from './base';
+
+export default class NotFound extends Exception {
+  constructor(message: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = message || 'Not found';
+  }
+}
