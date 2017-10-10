a52766a639dd07ffc2fc0884c6ca4aefbd2ec74e
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-10 15:23:04 +0200

    feat(classes/exceptions/over_quota.js) class created

---
+++ src/classes/exceptions/over_quota.js
@@ -0,0 +1,10 @@
+// @flow
+import Exception from './base';
+
+export default class OverQuota extends Exception {
+  constructor(message: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = message || 'Over quota';
+  }
+}
