9a3ca66332698169736a3f7f939b9a8adf89ef1d
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-10 15:18:33 +0200

    feat(classes/exceptions/bad_request.js) class created

---
+++ src/classes/exceptions/bad_request.js
@@ -0,0 +1,10 @@
+// @flow
+import Exception from './base';
+
+export default class BadRequest extends Exception {
+  constructor(message: string) {
+    super();
+    this.name = this.constructor.name;
+    this.message = message || 'Bad request';
+  }
+}
