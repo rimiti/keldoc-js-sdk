680f18d706e7492b1f80566e2b0e69192eae14a4
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-10 15:16:16 +0200

    feat(classes/exceptions/base.js) class created

---
+++ src/classes/exceptions/base.js
@@ -0,0 +1,18 @@
+// @flow
+export default class Exception {
+  get name(): string {
+    return this.name;
+  }
+
+  set name(value: string) {
+    this.name = value;
+  }
+
+  get message(): string {
+    return this.message;
+  }
+
+  set message(value: string) {
+    this.message = value;
+  }
+}
