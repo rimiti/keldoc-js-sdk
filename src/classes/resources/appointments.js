e0ed0a0208373b23845e9b8794a805845c09d601
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-23 15:34:52 +0200

    style(classes/resources/appointment.js) code stylized

--- src/classes/resources/appointments.js
+++ src/classes/resources/appointments.js
@@ -5,12 +5,14 @@ import Validation from '../validation';
 export default class Appointments extends Common {
   create(body: { start_at: string }): Promise<any> {
     const url = `${this.configuration.routes.appointments}`;
-    return Validation.validateCreateAppointment(body).then(() => super.postRequest(url, body));
+    return Validation.validateCreateAppointment(body)
+      .then(() => super.postRequest(url, body));
   }
 
   update(id: number, body: { start_at: string }): Promise<any> {
     const url = `${this.configuration.routes.appointments}/${id.toString()}.json`;
-    return Validation.validateUpdateAppointment(body).then(() => super.putRequest(url, body));
+    return Validation.validateUpdateAppointment(body)
+      .then(() => super.putRequest(url, body));
   }
 
   remove(id: number): Promise<any> {
