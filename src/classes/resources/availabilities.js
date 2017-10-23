fbbeb13313c0b5e43cdbe79be7b9e0c314602998
rimiti <dimitri.dobairro@dimsolution.com>
2017-10-23 15:35:08 +0200

    style(classes/resources/availabilities.js) code stylized

--- src/classes/resources/availabilities.js
+++ src/classes/resources/availabilities.js
@@ -4,8 +4,9 @@ import Validation from '../validation';
 import type {fetchAvailabilitiesRequest} from '../types';
 
 export default class Availabilities extends Common {
-  get(body: fetchAvailabilitiesRequest) : Promise<any> {
+  get(body: fetchAvailabilitiesRequest): Promise<any> {
     const url = `${this.configuration.routes.availabilities}`;
-    return Validation.validateFetchAvailabilities(body).then(() => super.getRequest(url, body));
+    return Validation.validateFetchAvailabilities(body)
+      .then(() => super.getRequest(url, body));
   }
 }
