f03e3b137e0daaf793a879a9f4bd08d607c0eec9
rimiti <dimitri.dobairro@dimsolution.com>
2018-01-29 17:48:14 +0100

    feat(resources/available_slots.js) type changed

--- src/classes/resources/available_slots.js
+++ src/classes/resources/available_slots.js
@@ -1,7 +1,7 @@
 // @flow
 import Common from '../common';
 import Validation from '../validation';
-import type {fetchAvailabilitiesRequest} from '../types';
+import type {fetchAvailableSlotsRequest} from '../types';
 
 export default class AvailableSlots extends Common {
   /**
@@ -9,7 +9,7 @@ export default class AvailableSlots extends Common {
    * @param body
    * @return {Promise.<any>}
    */
-  get(body: fetchAvailabilitiesRequest): Promise<any> {
+  get(body: fetchAvailableSlotsRequest): Promise<any> {
     const url = `${this.configuration.routes.available_slots}`;
     return Validation.validateFetchAvailableSlots(body)
       .then(() => super.getRequest(url, body));
