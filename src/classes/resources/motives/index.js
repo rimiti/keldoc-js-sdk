85bd126c478d946544f493cc263d20813a631715
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-04-30 17:21:38 +0200

    feat(classes/resource/motives/index.js) can pass parameters

--- src/classes/resources/motives/index.js
+++ src/classes/resources/motives/index.js
@@ -1,13 +1,14 @@
 // @flow
 import Common from '../../common';
+import type {fetchMotivesRequest} from '../../types';
 
 export default class Motives extends Common {
   /**
    * @description Get motives.
    * @return {Promise.<any>}
    */
-  get(): Promise<any> {
+  get(options: fetchMotivesRequest): Promise<any> {
     const url = `${this.configuration.routes.motives}`;
-    return super.getRequest(url);
+    return super.getRequest(url, options);
   }
 }
