671e8dad09bf532b1e94d9ae1655dadb49034fae
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-03-07 12:32:27 +0100

    fix(classes/available_slots/index.js) only pass params

--- src/classes/resources/available_slots/index.js
+++ src/classes/resources/available_slots/index.js
@@ -6,12 +6,12 @@ import type {fetchAvailableSlotsRequest} from '../../types';
 export default class AvailableSlots extends Common {
   /**
    * @description Get availabilities.
-   * @param options
+   * @param params
    * @return {Promise.<any>}
    */
-  get(options: fetchAvailableSlotsRequest): Promise<any> {
+  get(params: fetchAvailableSlotsRequest): Promise<any> {
     const url = `${this.configuration.routes.availableSlots}`;
-    return Validation.validateFetchAvailableSlots(options)
-      .then(() => super.getRequest(url, options));
+    return Validation.validateFetchAvailableSlots(params)
+      .then(() => super.getRequest(url, params));
   }
 }
